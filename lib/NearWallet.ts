50d4c9a6551c259ac71cfc09bf24dfe7420fd8eb
ivannt-ux <onyemaebuka3@gmail.com>
2025-08-11 03:29:21 +0100

    Update NearWallet.ts

--- lib/NearWallet.ts
+++ lib/NearWallet.ts
@@ -1,11 +1,11 @@
 import { setupWalletSelector } from "@near-wallet-selector/core";
 import { setupModal } from "@near-wallet-selector/modal-ui";
-import { setupNearWallet } from "@near-wallet-selector/near-wallet";
+import { setupNearWallet } from "@near-wallet-selector/my-near-wallet";
 
 export async function initNearWallet() {
   const selector = await setupWalletSelector({
     network: "testnet",
-    modules: [setupNearWallet()],
+    modules: [setupMyNearWallet()],
   });
 
   const modal = setupModal(selector, { contractId: "example.testnet" });
